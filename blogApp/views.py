7236ae2684834f33d5a4c14fb1ab8b8b93c92e9a
baaserosman <baaser.osman@gmail.com>
2022-06-14 11:40:42 +0200

    permissions

--- blogApp/views.py
+++ blogApp/views.py
@@ -3,12 +3,14 @@ from rest_framework import generics
 from blogApp.models import Blog
 from blogApp.serializers import BlogSerializer
 from rest_framework.viewsets import ModelViewSet
+from .permissions import IsAdminOrReadOnly
 
 # class BlogList(generics.ListAPIView):
 #     serializer_class=BlogSerializer
 #     queryset = Blog.objects.all()
 
 class BlogCRUD(ModelViewSet):
+    permission_classes = [IsAdminOrReadOnly]
     queryset = Blog.objects.all()
     serializer_class=BlogSerializer
     
